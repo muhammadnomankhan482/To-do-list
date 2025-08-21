9f9fe729b655237d8e7b1a4deb9a082f7a7be8c6
Muhammad Noman Khan <muhammadnomankhan482@gmail.com>
2025-08-21 10:59:31 +0500

    nomi khan

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-var todos =[];
+var todos = [];
 var updateButton = document.getElementById("update-button");
 var addButton = document.getElementById("add-button");
-// var inputElement = getElementById("todo");
+var inputElement = document.getElementById("todo");
+var editIndex 
+
 function addTodo(){
-    var inputElement = document.getElementById("todo");
-    if(inputElement===""){
+    if(inputElement.value === ""){
         alert("Please add somethings")
+    return;
     }
     todos.push(inputElement.value);
     inputElement.value = "";
-    showList()
+    showList();
 }
 
 function showList(){
-    var listItem = document.getElementById("todo-list") 
-    listItem.innerHTML = ""
+    var listItem = document.getElementById("todo-list");
+    listItem.innerHTML = "";
     for(var i=0; i<todos.length; i++){
-        listItem.innerHTML +=`<li> ${todos[i]} <button onclick="deleteTodo(${i})">Delete </button></li>`
+        listItem.innerHTML +=`<li> ${todos[i]} <button id="edit-button" onclick="editTodo(${i})">Edit </button> <button id="delete-button" onclick="deleteTodo(${i})">Delete </button></li>`
     }
 }
+
 function deleteTodo(index){
-    todos.splice(index,1)
-    showList()
+    todos.splice(index,1);
+    showList();
 }
-showList()
+
+function editTodo(index){
+    inputElement.value = todos[index];
+    editIndex = index;
+    addButton.style.display = "none";
+    updateButton.style.display = "inline-block";
+}
+
+function updateTodo(){
+    if(inputElement.value === ""){
+        alert("Please add something!")
+        return;
+    }
+
+//update todo
+    todos[editIndex] = inputElement.value;
+    inputElement.value = "";
+
+//update button
+    addButton.style.display ="inline-block";
+    updateButton.style.display ="none";
+    showList();
+}
